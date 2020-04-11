5192a6f8ce23b70c6feb610eb9130c665fb40062
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-04-11 15:18:21 -0700

    Enable post from frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://codingthesmartway.com">
+            <a className="navbar-brand" href="https://codingthesmartway.com">
               <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
             </a>
             <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
